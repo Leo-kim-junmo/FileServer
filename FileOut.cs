32f9d5b64007bf7f3b3964add9baf67551ae886c
준모 김 <kimjunmo91@naver.com>
2020-11-13 09:40:18 +0900

    server 1.0.1

--- FileOut.cs
+++ FileOut.cs
@@ -3,10 +3,75 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Threading;
+using System.Net.Sockets;
+using System.Net;
 
 namespace FileServer
 {
     class FileOut
     {
+        private string dirPath = "";
+        private Socket sock;
+        private IPEndPoint ip;
+        public FileOut(string str, Socket socket, string path)
+        {
+            dirPath = path + "\\";
+            dirPath = dirPath + str;
+            sock = socket;
+            ip = (IPEndPoint)sock.RemoteEndPoint;
+            Run(dirPath);
+        }
+
+        public void Run(string filepath)
+        {
+            Thread t = new Thread(new ParameterizedThreadStart(sendthread));
+            t.IsBackground = true;
+            t.Start(filepath);
+        }
+
+        private void sendthread(object o)
+        {
+            try
+            {
+                string filepath = (string)o;
+                string[] p = filepath.Split('\\');
+                string filename = p[p.Count() - 1];
+                Socket socket=new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+                socket.Connect(IPAddress.Parse(ip.Address.ToString()), 12345);
+
+                FileStream fileStream = new FileStream(filepath, FileMode.Open, FileAccess.Read);
+
+                int fileLength = (int)fileStream.Length;
+                byte[] fileBuffer = BitConverter.GetBytes(fileLength);
+                socket.Send(fileBuffer);
+
+                int fileNameLength = (int)filename.Length;
+                fileBuffer = BitConverter.GetBytes(fileNameLength);
+                socket.Send(fileBuffer);
+
+                fileBuffer = Encoding.UTF8.GetBytes(filename);
+                socket.Send(fileBuffer);
+
+                int count = fileLength / 1024 + 1;
+
+                BinaryReader reader = new BinaryReader(fileStream);
+
+                for(int i=0; i<count; i++)
+                {
+                    fileBuffer = reader.ReadBytes(1024);
+
+                    socket.Send(fileBuffer);
+                }
+                reader.Close();
+                socket.Close();
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("파일 전송 오류");
+            }
+        }
     }
 }
--- Packet.cs
+++ Packet.cs
@@ -8,5 +8,21 @@ namespace FileServer
 {
     class Packet
     {
+        public Packet()
+        {
+
+        }
+        public string HereIsData(List<string> stringlist)
+        {
+            string str = null;
+
+            str += "HereIsData" + "\a";
+            str += stringlist.Count.ToString() + "@";
+            for(int i=0; i<stringlist.Count; i++)
+            {
+                str += stringlist[i] + "#";
+            }
+            return str;
+        }
     }
 }
